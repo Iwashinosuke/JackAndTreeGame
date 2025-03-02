e82e5739dbc434415b6e84ced8f71006e961b330
iwashinosuke <iwa.shintsubo@gmail.com>
2025-03-02 23:25:17 +0900

    SDLのバージョン更新

--- include/structs.hpp
+++ include/structs.hpp
@@ -3,9 +3,13 @@
 
 #include <SDL3/SDL.h>
 
-typedef struct App          app_t;
-typedef struct Mouse        mouse_t;
-typedef struct Keyboard     keyboard_t;
+typedef long long int           entityId;
+
+typedef struct App              app_t;
+typedef struct Mouse            mouse_t;
+typedef struct Keyboard         keyboard_t;
+typedef struct EntityManager    entity_manager_t;
+typedef struct Entity           entity_t;
 
 struct App
 {
@@ -26,6 +30,38 @@ struct Keyboard
     /* data */
 };
 
+/* loop内で更新されるオブジェクトはすべてこの構造体を継承すること */
+struct EntityManager
+{
+    public:
+        EntityManager();
+        ~EntityManager();
+        void init();
+        void event();
+        void iterate();
+        void Add(entity_t *entity);
+    private:
+        
 
+        void Add(entity_t *entity);
+        void remove_entity(entityId id);
+        entity_t *get_entity(entityId id);
+        void update();
+        void draw();
+        entity_manager_t *entity_manager;
+};
+
+struct Entity
+{
+    public:
+        Entity()
+        {
+            _id = 0;
+            _active = true;
+        }
+    private:
+        entityId _id;
+        bool _active;
+};
 
 #endif // STRUCTS_HPP
