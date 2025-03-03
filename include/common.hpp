b906db9b12d5c9f02ceaee6175a23ed76e3bbcec
iwashinosuke <iwa.shintsubo@gmail.com>
2025-03-03 20:33:50 +0900

    各パラメータを管理するヘッダファイル追加

---
+++ include/common.hpp
@@ -0,0 +1,15 @@
+namespace PlayerParam
+{
+    constexpr float MAX_HEIGHT = 100.0f;
+    constexpr float GRAVITY = 25.0f;
+    constexpr float JUMP_POWER = 100.0f;
+    constexpr float MAX_DX = 10.0f;
+    constexpr float AX = 2.0f;
+}
+
+namespace StageParam
+{
+    constexpr int WIDTH = 600;
+    constexpr int HEIGHT = 800;
+    constexpr int BLOCK_SIZE = 30;
+}
